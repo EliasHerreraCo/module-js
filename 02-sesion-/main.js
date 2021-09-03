e27dd4d11acf9fc77cb9162a6c0d56f82141fc25
Elias Herrera <elias.herrera.co@gmail.com>
2021-09-02 19:27:44 -0500

    Tarea de la sesion 2: ¿Cuanto pesarias en otros planetas?

---
+++ 02-sesion-/main.js
@@ -0,0 +1,14 @@
+let peso = prompt('¿Cuanto pesas? (en kg.)');
+const luna = 1.62;
+const marte = 3.721;
+const jupiter = 24.79;
+
+
+let resultadoLuna = peso ** luna;
+console.log(`Tu peso en la luna seria ${resultadoLuna} kg.`);
+
+let resultadoMarte = peso * marte;
+console.log(`Tu peso en marte seria ${resultadoMarte} kg.`);
+
+let resultadoJupiter = peso * jupiter;
+console.log(`Tu peso en jupiter seria ${resultadoJupiter} kg.`);
